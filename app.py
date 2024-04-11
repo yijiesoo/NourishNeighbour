818de5450fbf82cbbcc2ed84bff1b168de1d8b20
yijiesoo <sooyijie111@gmail.com>
2024-04-11 22:44:59 +0800

    fixed up a few bugs, now users can view the other users listings using chats.html

--- app.py
+++ app.py
@@ -552,14 +552,13 @@ def items():
 
 @app.route('/chats/<chatroom_id>')
 def chats(chatroom_id):
-    # Retrieve the profile_picture_url from the session
-    profile_picture_url = session.get('profile_picture_url', None)
-
-    # If profile_picture_url is not in the session, you might want to set a default value
-    if profile_picture_url is None:
-        profile_picture_url = url_for('static', filename='default-profile-image.png')
-
-    return render_template('chats.html', chatroom_id=chatroom_id, profile_picture_url=profile_picture_url)
+    current_user_id = session['user_id']
+    # Split the chatroom_id to get the two user IDs
+    user_ids = chatroom_id.split('_')
+    # Determine which user ID is not the current user
+    other_user_id = user_ids[0] if user_ids[0] != current_user_id else user_ids[1]
+    # Render the template with the chatroom_id and the other user's ID
+    return render_template('chats.html', chatroom_id=chatroom_id, other_user_id=other_user_id)
 
 def create_chatroom():
     # Ensure the user is logged in
